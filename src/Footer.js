28e56fa114ff5021705312af46af56e259a4e23b
Muhammad Khuzain <mkhuzain17@gmail.com>
2022-05-01 10:40:59 +0800

    add React.component (Header, MainComponent, Footer)

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <p>Copyright Khuzain 2022. All right reserved</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/MainComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function MainComponent() {
+    return (
+        <div>
+            <p>Why W interested in learning React.js?</p>
+            <ul>
+                <li>I want to upgrade my skill</li>
+                <li>I must learn React for my project</li>
+                <li>I want to improve my logic and durability in coding</li>
+                <li>I want to learning front end web dev</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MainComponent
